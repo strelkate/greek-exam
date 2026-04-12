a11ed9a84282e516fb9a0677cef7c664dd621d25
e.vasileva <e.vasileva@3h.ru>
2026-04-13 00:09:42 +0300

    test: add models importability test

---
+++ backend/tests/test_models_import.py
@@ -0,0 +1,10 @@
+def test_models_importable():
+    from app.models.enums import LevelEnum, ExerciseTypeEnum, CardStatusEnum
+    from app.models.user import User
+    from app.models.curriculum import CurriculumUnit
+    from app.models.exercise import Exercise, PlacementTestQuestion
+    from app.models.vocabulary import VocabularyCard, UserCardState
+    from app.models.progress import UserProgress
+    from app.models.xp_log import XpLog
+    assert LevelEnum.A1 == "A1"
+    assert ExerciseTypeEnum.TRUE_FALSE == "true_false"
